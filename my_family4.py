c1e09cb0bcee67e5ec4c6e1a106f72a495f30e2d
zxceys <grishasurgut111@gmail.com>
2025-02-20 11:51:42 +0500

    medium

--- my_family4.py
+++ my_family4.py
@@ -19,7 +19,7 @@ my_family_height = [
 ]
 # Выведите на консоль рост отца в формате
 #   Рост отца - ХХ см
-print(my_family_height[1][1])
+print('Рост отца -',my_family_height[1][1], 'см')
 # Выведите на консоль общий рост вашей семьи как сумму ростов всех членов
 #   Общий рост моей семьи - ХХ см
-print(my_family_height[0][1] + my_family_height[1][1]+ my_family_height[2][1])
+print('Общий рост моей семьи - ', my_family_height[0][1] + my_family_height[1][1]+ my_family_height[2][1], 'см')
