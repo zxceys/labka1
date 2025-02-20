66e01852b06ace8c481c424cffecfbe7f8b9f1c2
zxceys <grishasurgut111@gmail.com>
2025-02-20 02:16:52 +0000

    medium

---
+++ IMPORT1.py
@@ -0,0 +1,12 @@
+from distance0 import distances
+from circle1 import s
+from operations2 import s
+from favorite_movies3 import my_favorite_movies
+from my_family4 import my_family_height
+from zoo5 import zoo
+from songs_list6 import b
+from secret7 import s1
+from garden8 import gm_set
+from shopping9 import sweets
+from store10 import table_quantity
+
